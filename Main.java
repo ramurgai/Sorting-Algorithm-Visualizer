f67a224d6b43c36a1a6d98d7f72d949deef31d78
ramurgai <ramurgai@my.waketech.edu>
2026-04-22 10:15:39 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,12 +9,7 @@ public class Main {
         int[] nums = new int[30];
         fillList(nums);
         System.out.println(Arrays.toString(nums));
-        System.out.println("Phil Collins Sort: ");
-        philCollinsSort(nums);
-        System.out.println(Arrays.toString(nums));
-
-
-
+        assasinSort(nums);
 
     }
 
@@ -61,51 +59,83 @@ public class Main {
             int currentElement = list[i];
             int k = i - 1;
 
-            if(list[i] > average){
-                list[i] *= 2;
+            if(currentElement >= average){
+                currentElement *= 2;
             }
-
+            else{
+                currentElement *= -2;
+            }
             while (k >= 0 && (list[k] - currentElement) > 0) {
                 list[k + 1] = list[k];
                 k--;
             }
-
             list[k + 1] = currentElement;
         }
-
-
     }
 
-    public static <E extends Comparable<E>> void communismSort(E[] list) {
+    public static void communismSort(int[] list) {
         //all the elements get evenly distributed
+        int total = 0;
+        for(int num: list) {total+= num;}
+        int average = (total / list.length);
+
+        for(int i = 0; i < list.length; i++){
+            list[i] = average;
+            System.out.println("Iteration" + i + ": " + Arrays.toString(list));
+        }
     }
 
-    public static <E extends Comparable<E>> void assasinSort(E[] list) {
+    public static void assasinSort(int[] list) {
         //if the element in the next position is not larger it gets deleted
+        ArrayList<Integer> adaptableList = new ArrayList<>(list.length);
+        for(int val: list){adaptableList.add(val);}
+
+
+        for (int i = 0; i < adaptableList.size(); i++){
+            int currentElement = adaptableList.get(i);
+            System.out.println("Current index: " + i);
+            int k = i - 1;
+
+            if(adaptableList.get(i) > adaptableList.get(i + 1)){
+                while (k >= 0 && (adaptableList.get(k) - currentElement) > 0) {
+                    adaptableList.set(k + 1, k);
+                    k--;
+                }
+                list[k + 1] = currentElement;
+                adaptableList.set(k + 1, currentElement);
+                System.out.println("Didn't remove anything");
+            }
+            else {
+                System.out.println("Removing element: " + adaptableList.get(i));
+                adaptableList.remove(i);
+            }
+            System.out.println("Iteration " + i + ": " + adaptableList);
+        }
+
     }
 
-    public static <E extends Comparable<E>> void carrierPigeonSort(E[] list) {
+    public static void carrierPigeonSort(int[] list) {
         //take the element out of the array, encapsulates it, deencapsulates it, then places it in the sorted position
     }
 
-    public static <E extends Comparable<E>> void dmvSort(E[] list) {
+    public static void dmvSort(int[] list) {
         //you have to answer a series of questions and give your DLN and if you can't do that it won't sort your array
         //even if it sorts your array it'll take a while
     }
 
-    public static <E extends Comparable<E>> void infiniteImperminanceSort(E[] list) {
+    public static void infiniteImperminanceSort(int[] list) {
         //generates number of X^10 where x is the size of the array and recurses
     }
 
-    public static <E extends Comparable<E>> void adhdSort(E[] list) {
+    public static void adhdSort(int[] list) {
         //sort but it gets distracted randomly and takes a while to sort
     }
 
-    public static <E extends Comparable<E>> void sisyphusSort(E[] list) {
+    public static void sisyphusSort(int[] list) {
         //tries to sort the array but at the very end a new random element is inserted at the front
     }
 
-    public static <E extends Comparable<E>> void medusaSort(E[] list) {
+    public static void medusaSort(int[] list) {
         //if the element 1 for I is in the array the sort stops
     }
 
