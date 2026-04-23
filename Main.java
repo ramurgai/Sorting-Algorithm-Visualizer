ee7eb4d2bf22638748e0c0b136c5960ea71a9b33
ramurgai <ramurgai@my.waketech.edu>
2026-04-22 21:57:53 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,12 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
         int[] nums = new int[30];
         fillList(nums);
         System.out.println(Arrays.toString(nums));
-        assasinSort(nums);
+        sisyphusSort(nums);
 
     }
 
@@ -88,55 +85,78 @@ public class Main {
     public static void assasinSort(int[] list) {
         //if the element in the next position is not larger it gets deleted
         ArrayList<Integer> adaptableList = new ArrayList<>(list.length);
-        for(int val: list){adaptableList.add(val);}
+        int maxVal = list[0];
 
-
-        for (int i = 0; i < adaptableList.size(); i++){
+        for (int i = 0; i < list.length; i++){
+            if (list[i] >= maxVal) {
+                adaptableList.add(list[i]);
+                maxVal = list[i];
+            }
+        }
+        
+        for (int i = 1; i < adaptableList.size(); i++){
             int currentElement = adaptableList.get(i);
-            System.out.println("Current index: " + i);
             int k = i - 1;
-
-            if(adaptableList.get(i) > adaptableList.get(i + 1)){
-                while (k >= 0 && (adaptableList.get(k) - currentElement) > 0) {
-                    adaptableList.set(k + 1, k);
+            
+            while (k >= 0 && (adaptableList.get(k) - currentElement) > 0) {
+                    adaptableList.set(k + 1, adaptableList.get(k));
                     k--;
                 }
                 list[k + 1] = currentElement;
                 adaptableList.set(k + 1, currentElement);
-                System.out.println("Didn't remove anything");
-            }
-            else {
-                System.out.println("Removing element: " + adaptableList.get(i));
-                adaptableList.remove(i);
-            }
-            System.out.println("Iteration " + i + ": " + adaptableList);
         }
 
     }
 
-    public static void carrierPigeonSort(int[] list) {
-        //take the element out of the array, encapsulates it, deencapsulates it, then places it in the sorted position
-    }
 
-    public static void dmvSort(int[] list) {
-        //you have to answer a series of questions and give your DLN and if you can't do that it won't sort your array
-        //even if it sorts your array it'll take a while
-    }
-
-    public static void infiniteImperminanceSort(int[] list) {
-        //generates number of X^10 where x is the size of the array and recurses
-    }
-
-    public static void adhdSort(int[] list) {
-        //sort but it gets distracted randomly and takes a while to sort
-    }
 
     public static void sisyphusSort(int[] list) {
         //tries to sort the array but at the very end a new random element is inserted at the front
+        ArrayList<Integer> adaptableList = new ArrayList<>();
+        for (int val : list) {
+            adaptableList.add(val);
+        }
+        
+        while (true) {
+            for (int i = 1; i < adaptableList.size(); i++){
+            int currentElement = adaptableList.get(i);
+            int k = i - 1;
+            
+            while (k >= 0 && (adaptableList.get(k) - currentElement) > 0) {
+                    adaptableList.set(k + 1, adaptableList.get(k));
+                    k--;
+                }
+                adaptableList.set(k + 1, currentElement);
+            }
+            int newElement = (int)(Math.random() * 101);
+            adaptableList.add(0, newElement);
+            System.out.println(adaptableList);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            
+        }
     }
 
     public static void medusaSort(int[] list) {
         //if the element 1 for I is in the array the sort stops
+        for (int i = 1; i < list.length; i++){
+            if (list[i] == 1){
+                break;
+            }
+            int currentElement = list[i];
+            int k = i - 1;
+
+            while (k >= 0 && (list[k] - currentElement) > 0) {
+                list[k + 1] = list[k];
+                k--;
+            }
+
+            list[k + 1] = currentElement;
+            System.out.println("Iteration" + i + ": " + Arrays.toString(list));
+        }
     }
 
 }
